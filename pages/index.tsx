11cbf91559e2e2c40cefc110f95a30114d75b3ae
HS88_LHJ <dlgudwns@hs88.kr>
2023-03-22 12:40:05 +0900

    set test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [state, setState] = useState("No data");
+
+  const callApiTest = async (): Promise<any> => {
+    const res = await fetch("/api/hello");
+    return res.json();
+  };
+
+  const bindingData = useCallback(async () => {
+    const data = await callApiTest();
+    console.log("data", data.name);
+    setState(data.name);
+  }, []);
+
+  useEffect(() => {
+    bindingData();
+  }, [bindingData]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +35,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -60,12 +78,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
